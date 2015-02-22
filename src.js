74960e7dd9ed645cd576d4af58cd85ad5414b590
Zack Gold <zzatkin@gmail.com>
2015-02-22 18:08:39 -0500

    making src.js more readable and src.min.js less readable

--- src.js
+++ src.js
@@ -1,28 +1,28 @@
 function forEach(arr, func) {
     for(var i = 0; i < arr.length; i++)
-        func(arr[i]);
+        func(arr[i])
 }
 
 function filter(arr, test) {
     var ret = [];
     for(var i = 0; i < arr.length; i++)
         if(test(arr[i]))
-            ret.push(arr[i]);
-    return ret;
+            ret.push(arr[i])
+    return ret
 }
 
 function map(arr, func) {
     var ret = [];
     for(var i = 0; i < arr.length; i++)
-        ret.push(func(arr[i]));
-    return ret;
+        ret.push(func(arr[i]))
+    return ret
 }
 
 function reduce(arr, func, init) {
     var ret = init;
     for(var i = 0; i < arr.length; i++)
-        ret = func(ret, arr[i]);
-    return ret;
+        ret = func(ret, arr[i])
+    return ret
 }
 
 var tests = [
@@ -31,4 +31,4 @@ var tests = [
     reduce([1,2,3,4],function(x,y){return x+y},0)
 ];
 
-forEach(tests,function(x){console.log(x)});
+forEach(tests,function(x){console.log(x)})
