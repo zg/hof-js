f862c1abb6f0455a94c9c632a5635c52387ef9e1
Zack Zatkin-Gold <zzatkin@gmail.com>
2015-03-21 15:51:01 -0400

    adding average, every, groupBy, none, reverse, and some; replaced many for loops with for-in loops; added verbosity for src.js since it's not minified

--- src.js
+++ src.js
@@ -1,34 +1,110 @@
-function forEach(arr, func) {
-    for(var i = 0; i < arr.length; i++)
-        func(arr[i])
+average = function(arr) {
+    return reduce(arr,function(a,b) {
+        return a + b;
+    }, 0) / arr.length;
 }
 
-function filter(arr, test) {
+every = function(arr, has) {
+    for(item in arr) {
+        if(arr[item] != has) {
+            return false;
+        }
+    }
+    return true;
+}
+
+forEach = function(arr, func) {
+    for(item in arr) {
+        func(arr[item]);
+    }
+}
+
+filter = function(arr, pred) {
     var ret = [];
-    for(var i = 0; i < arr.length; i++)
-        if(test(arr[i]))
-            ret.push(arr[i])
-    return ret
+    for(item in arr) {
+        if(pred(arr[item])) {
+            ret.push(arr[item]);
+        }
+    }
+    return ret;
 }
 
-function map(arr, func) {
+groupBy = function(arr, grouping) {
+  ret = [];
+  for(item in arr) {
+      group = grouping(arr[item]);
+      if(!ret[group]) {
+          ret[group] = [];
+      }
+      ret[group].push(arr[item]);
+  }
+  return ret;
+}
+
+map = function(arr, func) {
     var ret = [];
-    for(var i = 0; i < arr.length; i++)
-        ret.push(func(arr[i]))
-    return ret
+    for(item in arr) {
+        ret.push(func(arr[item]));
+    }
+    return ret;
 }
 
-function reduce(arr, func, init) {
+none = function(arr, has) {
+    for(item in arr) {
+        if(arr[item] === has) {
+            return false;
+        }
+    }
+    return true;
+}
+
+reduce = function(arr, func, init) {
     var ret = init;
-    for(var i = 0; i < arr.length; i++)
-        ret = func(ret, arr[i])
-    return ret
+    for(item in arr) {
+        ret = func(ret, arr[item]);
+    }
+    return ret;
+}
+
+reverse = function(arr) {
+    half = arr.length >> 1;
+    for(item in arr){
+        if(item < half) {
+            arr[arr.length - item - 1] ^= arr[item];
+            arr[item] ^= arr[arr.length - item - 1];
+            arr[arr.length - item - 1] ^= arr[item];
+        }
+    }
+    return arr;
+}
+
+some = function(arr, has) {
+    for(item in arr) {
+        if(arr[item] == has) {
+            return true;
+        }
+    }
+    return false;
 }
 
 var tests = [
-    filter([1,2,3,4],function(x){return x%2==0}),
-    map([1,2,3,4],function(x){return x*x}),
-    reduce([1,2,3,4],function(x,y){return x+y},0)
+    average([1,2,3,4]),
+    every([1,1,1,1], 1),
+    filter([1,2,3,4], function(x) {
+        return x % 2 == 0;
+    }),
+    groupBy([1,2,3,4], function(x) {
+        return x % 2 == 0;
+    }),
+    map([1,2,3,4], function(x) {
+        return x * x;
+    }),
+    none([1,2,3,4], 5),
+    reduce([1,2,3,4], function(x, y) {
+        return x + y;
+    }, 0),
+    reverse([1,2,3,4,5]),
+    some([1,2,3,4], 3)
 ];
 
 forEach(tests,function(x){console.log(x)})
