53b182f1bbaf78acbd99d7b455390465bdca0b7a
Zack Zatkin-Gold <zzatkin@gmail.com>
2015-03-28 17:01:15 -0400

    adding _assert, fold, foldRight, range, and sum; modifying reduce and reduceRight (fold requires initial value, reduce doesn't); separating tests into separate javascript file; separating tests for hof.min.js and hof.js

--- hof.js
+++ hof.js
@@ -1,113 +1,245 @@
 /**
  * hof.js by github.com/zg
  */
+
+/**
+ * _assert - a helper that throws an exception if condition is not met
+ * @param condition the condition to check
+ * @param the message to display (optional)
+ */
+_assert = function(condition, message) {
+    if (!condition) {
+        var message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
+
+/**
+ * average - compute the average of a given array of integers
+ * @param arr the array containing integers
+ * @return average of the values
+ */
 average = function(arr) {
-    return reduce(arr,function(a,b) {
+    return reduce(arr, function(a, b) {
         return a + b;
     }, 0) / arr.length;
 }
 
-every = function(arr, has) {
-    for(item in arr) {
-        if(arr[item] != has) {
+/**
+ * every - checks if every element in arr has item
+ * @param item the item to check
+ * @return true if all items in arr are item, else false
+ */
+every = function(arr, item) {
+    for(index in arr) {
+        if(arr[index] !== item) {
             return false;
         }
     }
     return true;
 }
 
-forEach = function(arr, func) {
-    for(item in arr) {
-        func(arr[item]);
-    }
-}
-
+/**
+ * filter - return an array of items that meet a predicate
+ * @param arr the array
+ * @param pred the predicate
+ * @return a filtered array
+ */
 filter = function(arr, pred) {
     var ret = [];
-    for(item in arr) {
-        if(pred(arr[item])) {
-            ret.push(arr[item]);
+    for(index in arr) {
+        if(pred(arr[index])) {
+            ret.push(arr[index]);
         }
     }
     return ret;
 }
 
+/**
+ * fold - aggregates all elements of an array based on a function, starting
+ *        with some initial value
+ * @param arr the array
+ * @param func the function
+ * @param init the initial value
+ * @return the aggregation of all elements
+ */
+fold = function(arr, func, init) {
+    var ret = init;
+    for(index in arr) {
+        ret = func(ret, arr[index]);
+    }
+    return ret;
+}
+
+/**
+ * foldRight - aggregates all elements of an array, starting at the last
+ *             index with an initial value, based on a function
+ * @param arr the array
+ * @param func the function
+ * @param init the initial value
+ * @return the aggregation of all elements
+ */
+foldRight = function(arr, func, init) {
+    var ret = init;
+    for(index in arr) {
+        ret = func(ret, arr[arr.length - index - 1]);
+    }
+    return ret;
+}
+
+/**
+ * foreach - applies func to each element of arr
+ * @param arr the array
+ * @param func the function to run
+ */
+forEach = function(arr, func) {
+    var ret = [];
+    for(index in arr) {
+        ret.push(func(arr[index]));
+    }
+    return ret;
+}
+
+/**
+ * groupBy - group elements of an array based on a grouping function
+ * @param arr the array
+ * @param grouping the grouping function
+ * @return an array containing arrays of groupings
+ */
 groupBy = function(arr, grouping) {
-  ret = [];
-  for(item in arr) {
-      group = grouping(arr[item]);
-      if(!ret[group]) {
-          ret[group] = [];
-      }
-      ret[group].push(arr[item]);
+  var ret = [];
+  for(index in arr) {
+    group = grouping(arr[index]);
+    if(!ret[group]) {
+      ret[group] = [];
+    }
+    ret[group].push(arr[index]);
   }
   return ret;
 }
 
+/**
+ * map - map an array of elements to values based on a function
+ * @param arr the array
+ * @param func the mapping function
+ * @return an array containing the mapping
+ */
 map = function(arr, func) {
     var ret = [];
-    for(item in arr) {
-        ret.push(func(arr[item]));
+    for(index in arr) {
+        ret.push(func(arr[index]));
     }
     return ret;
 }
 
-none = function(arr, has) {
-    for(item in arr) {
-        if(arr[item] === has) {
+/**
+ * none - check if all elements in an array are not equal to item
+ * @param arr the array
+ * @param item the item to check
+ * @return true if none of the elements match, else false
+ */
+none = function(arr, item) {
+    for(index in arr) {
+        if(arr[index] === item) {
             return false;
         }
     }
     return true;
 }
 
-reduce = function(arr, func, init) {
-    var ret = init;
-    for(item in arr) {
-        ret = func(ret, arr[item]);
+/**
+ * range - build a range of numbers between start and end, exclusive
+ * @param start lower bound
+ * @param end upper bound
+ * @return an array of numbers between start and end, exclusive
+ */
+range = function(start, end, step) {
+    _assert(!(step === 0), "Cannot take a step");
+    var step = step || 1;
+    _assert(!(start < end && step < 0), "Invalid range (infinite loop)");
+    _assert(!(end < start && 0 < step), "Invalid range (infinite loop)");
+    var ret = [];
+    if(0 < step) {
+        for(ret.push(start); ret[ret.length - 1] + step < end; ret.push(ret[ret.length - 1] + step));
+    } else {
+        for(ret.push(start); ret[ret.length - 1] + step > end; ret.push(ret[ret.length - 1] + step));
     }
     return ret;
 }
 
+/**
+ * reduce - aggregates all elements of an array based on a function
+ * @param arr the array
+ * @param func the function
+ * @return the aggregation of all elements
+ */
+reduce = function(arr, func) {
+    return fold(arr, func, 0);
+}
+
+/**
+ * reduceRight - aggregates all elements of an array, starting at the last
+ *               index, based on a function
+ * @param arr the array
+ * @param func the function
+ * @return the aggregation of all elements
+ */
+reduceRight = function(arr, func) {
+    return foldRight(arr, func, 0);
+}
+
+/**
+ * reverse - reverse elements in an array
+ * @param arr the array
+ * @return the reversed array
+ */
 reverse = function(arr) {
-    half = arr.length >> 1;
-    for(item in arr){
-        if(item < half) {
-            arr[arr.length - item - 1] ^= arr[item];
-            arr[item] ^= arr[arr.length - item - 1];
-            arr[arr.length - item - 1] ^= arr[item];
+    var half = arr.length >> 1;
+    for(index in arr) {
+        if(index < half) {
+            arr[arr.length - index - 1] ^= arr[index];
+            arr[index] ^= arr[arr.length - index - 1];
+            arr[arr.length - index - 1] ^= arr[index];
         }
     }
     return arr;
 }
 
-some = function(arr, has) {
-    for(item in arr) {
-        if(arr[item] == has) {
+/**
+ * some - check if an array has some element in it
+ * @param arr the array
+ * @param item the item to check
+ * @return true if the array contains item, else false
+ */
+some = function(arr, item) {
+    for(index in arr) {
+        if(arr[index] == item) {
             return true;
         }
     }
     return false;
 }
 
-var tests = [
-    average([1,2,3,4]),
-    every([1,1,1,1], 1),
-    filter([1,2,3,4], function(x) {
-        return x % 2 == 0;
-    }),
-    groupBy([1,2,3,4], function(x) {
-        return x % 2 == 0;
-    }),
-    map([1,2,3,4], function(x) {
-        return x * x;
-    }),
-    none([1,2,3,4], 5),
-    reduce([1,2,3,4], function(x, y) {
-        return x + y;
-    }, 0),
-    reverse([1,2,3,4,5]),
-    some([1,2,3,4], 3)
-];
-
-forEach(tests,function(x){console.log(x)})
+/**
+ * sum - summation for all values of function from start to end
+ * @param func the function
+ * @param start the starting value
+ * @param end the ending value
+ * @return the summation of all values between start and end
+ */
+sum = function(start, end, func) {
+    var func = func || function(x) {
+        return x;
+    };
+    loop = function(start, acc) {
+        if(start > end) {
+            return acc;
+        } else {
+            return loop(start + 1, func(start) + acc);
+        }
+    }
+    return loop(start, 0);
+}
---
+++ hof.tests.js
@@ -0,0 +1,73 @@
+arraysEqual = function(a, b) {
+    if (a === b)
+        return true;
+    if (a == null || b == null)
+        return false;
+    if (a.length != b.length)
+        return false;
+    for(index in a) {
+        if(Object.prototype.toString.call(a[index]) == "[object Array]") {
+            if(!arraysEqual(a[index],b[index]))
+                return false;
+        }
+        else if (a[index] !== b[index])
+            return false;
+    }
+    return true;
+}
+
+objToArray = function(obj) {
+    var ret = [];
+    for(item in obj)
+        ret[item] = obj[item];
+    return ret;
+}
+
+var tests = [
+    ["average",average([1,2,3,4]),2.5],
+    ["every",every([1,1,1,1],1),true],
+    ["filter",filter([1,2,3,4],function(x){return x % 2 == 0}),[2,4]],
+    ["fold",fold(['1',2,'3',4,5],function(a,b){return a+b},0),"012345"],
+    ["foldRight",foldRight(['1',2,'3',4,5],function(a,b){return a+b},0),"9321"],
+    ["forEach",forEach([1,2,3,4],function(x){return x*x*x}),[1,8,27,64]],
+    ["groupBy",groupBy([3.14,3.65,10,10.4,15],function(x){return Math.round(x)}),objToArray({3:[3.14],4:[3.65],10:[10,10.4],15:[15]})],
+    ["map",map(["John","Apple","Seed"],function(x){return x.length}),[4,5,4]],
+    ["none",none([0,2,3,4,5],1),true],
+    ["range",range(5,3,-3),[5]],
+    ["reduce",reduce([1,2,3,4],function(a,b){return a+b}),10],
+    ["reduceRight",reduceRight(['1',2,'3',4,5],function(a,b){return a+b}),"9321"],
+    ["reverse",reverse([1,2,3,4,5]),[5,4,3,2,1]],
+    ["some",some([1,2,3,4,5],2),true],
+    ["sum",sum(1,5,function(x){return x*x*x}),225]
+];
+
+var test_count = tests.length;
+var pass_count = 0;
+
+for(test in tests) {
+    var passed = false;
+    var name = tests[test][0];
+    var result = tests[test][1];
+    var expect = tests[test][2];
+    var result_type = Object.prototype.toString.call(result);
+    var expect_type = Object.prototype.toString.call(expect);
+    var type_mismatch = result_type !== expect_type;
+    switch(Object.prototype.toString.call(expect)) {
+        case "[object Array]":
+            if(arraysEqual(result,expect)) {
+                passed = true;
+                pass_count++;
+            }
+            console.log("[" + (passed ? "PASS" : "FAIL") + "] " + name + ", expect: [" + expect + "], result: [" + result + "]" + (type_mismatch ? " (type mismatch)" : ""));
+        break;
+        default:
+            if(result === expect) {
+                passed = true;
+                pass_count++;
+            }
+            console.log("[" + (passed ? "PASS" : "FAIL") + "] " + name + ", expect: " + expect + ", result: " + result + (type_mismatch ? " (type mismatch)" : ""));
+        break;
+    }
+}
+
+console.log(pass_count+"/"+test_count+" tests passed.");
